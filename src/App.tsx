13e80c345ac0502d5e97ff2ee19823ee3269e7ad
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-06-28 11:51:59 -0300

    feature/update-routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 import { BrowserRouter } from 'react-router-dom'
+import { Router } from "./Router"
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-      
-      </BrowserRouter>
-
+        <BrowserRouter>
+          <Router/>        
+        </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
