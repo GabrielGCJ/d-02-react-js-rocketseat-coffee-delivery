aa936a69d9daf5f819217a469b9b308d79fe6f8b
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-06-28 12:34:56 -0300

    feature/add-fonts

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 
-// import { DefaultLayout } from './layouts/DefaultLayout/DefaultLayout'
+import { DefaultLayout } from './layouts/DefaultLayout/DefaultLayout'
 
 import { HomePage } from './pages/HomePage/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage/CheckoutPage'
@@ -9,11 +9,11 @@ import { SuccessPage } from './pages/SuccessPage/SucessPage'
 export const Router = () => {
     return (
       <Routes>
-        {/* <Route path="/" element={<DefaultLayout />}> */}
+        <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/success" element={<SuccessPage />} />
-        {/* </Route> */}
+        </Route>
       </Routes>
     )
   }
