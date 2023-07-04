49aa797db1ffdccae51a3e7fa058384d32e40aeb
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-04 12:56:06 -0300

    feature/add-index-assents

---
+++ src/assents/index.ts
@@ -0,0 +1,53 @@
+import shoppingCartImg from './shoppingCartSimplePurple.svg'
+import imgIntro from './imgIntro.svg'
+import iconLocation from './iconLocation.svg'
+import coffeeDeliveryLogo from './coffeeDeliveryLogo.svg'
+import buttonCart from './buttonCart.svg'
+
+import tag1tradicional from './coffee-tags/tag1-tradicional.svg'
+import tag2gelado from './coffee-tags/tag2-gelado.svg'
+import tag3comLeite from './coffee-tags/tag3-comLeite.svg'
+import tag4especial from './coffee-tags/tag4-especial.svg'
+import tag5alcoolico from './coffee-tags/tag5-alcoolico.svg'
+
+import arabeImg from './coffee-types/arabe.svg'
+import cafeComLeiteImg from './coffee-types/cafe-com-leite.svg'
+import capuccinoImg from './coffee-types/capuccino.svg'
+import chocolateQuenteImg from './coffee-types/chocolate-quente.svg'
+import cubanoImg from './coffee-types/cubano.svg'
+import expressoAmericanoImg from './coffee-types/expresso-americano.svg'
+import expressoCremosoImg from './coffee-types/expresso-cremoso.svg'
+import expressoGeladoImg from './coffee-types/expresso-gelado.svg'
+import expressoTradicionalImg from './coffee-types/expresso-tradicional.svg'
+import havaianoImg from './coffee-types/havaiano.svg'
+import irlandesImg from './coffee-types/irlandes.svg'
+import latteImg from './coffee-types/latte.svg'
+import macchiatoImg from './coffee-types/macchiato.svg'
+import mochaccinoImg from './coffee-types/mochaccino.svg'
+
+export {
+  shoppingCartImg,
+  imgIntro,
+  iconLocation,
+  coffeeDeliveryLogo,
+  buttonCart,
+  tag1tradicional,
+  tag2gelado,
+  tag3comLeite,
+  tag4especial,
+  tag5alcoolico,
+  arabeImg,
+  cafeComLeiteImg,
+  capuccinoImg,
+  chocolateQuenteImg,
+  cubanoImg,
+  expressoAmericanoImg,
+  expressoCremosoImg,
+  expressoGeladoImg,
+  expressoTradicionalImg,
+  havaianoImg,
+  irlandesImg,
+  latteImg,
+  macchiatoImg,
+  mochaccinoImg,
+}
