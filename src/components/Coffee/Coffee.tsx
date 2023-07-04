90456ba842f030d3492b1a660c08f36e9397fd04
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-03 23:58:01 -0300

    feature/start-css-coffee-component

--- src/components/Coffee/Coffee.tsx
+++ src/components/Coffee/Coffee.tsx
@@ -1,10 +1,32 @@
-import { CoffeeContainer, TagsContainer } from './styles'
 import coffeTradicional from '../../assents/coffee-types/expresso-tradicional.svg'
+import traditionalTag from '../../assents/coffee-tags/tag1-tradicional.svg'
+import geladoTag from '../../assents/coffee-tags/tag2-gelado.svg'
+
+import {
+  CoffeeExternalContainer,
+  CoffeeInternalContainer,
+  DescriptionContainer,
+  TagsContainer,
+  TitleContainer,
+} from './styles'
+
 export const Coffee = () => {
   return (
-    <CoffeeContainer>
-      <img src={coffeTradicional} alt="" />
-      <TagsContainer></TagsContainer>
-    </CoffeeContainer>
+    <CoffeeExternalContainer>
+      <CoffeeInternalContainer>
+        <img src={coffeTradicional} alt="" />
+        <TagsContainer>
+          <img src={traditionalTag} alt="" />
+          <img src={geladoTag} alt="" />
+          <img src={geladoTag} alt="" />
+        </TagsContainer>
+        <TitleContainer>
+          <h2>Expresso Tradicional</h2>
+        </TitleContainer>
+        <DescriptionContainer>
+          <p>O tradicional café feito com água quente e grãos moídos</p>
+        </DescriptionContainer>
+      </CoffeeInternalContainer>
+    </CoffeeExternalContainer>
   )
 }
