990b692c09dfdbea5ac280d3972ef4b05208e144
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-05 17:45:22 -0300

    feature/update-coffee-list

--- src/components/CoffeeList/CoffeeList.tsx
+++ src/components/CoffeeList/CoffeeList.tsx
@@ -1,17 +1,40 @@
-import { CoffeeData } from '../../data/CoffeeData'
+// import { CoffeeData } from '../../data/CoffeeData'
+import { CoffeeData } from '../../data/coffeeData'
 import { Coffee } from '../Coffee/Coffee'
 import {
+  AllCoffees,
   CoffeeListExternalContainer,
   CoffeeListInternalContainer,
+  CoffeeText,
 } from './styled'
 
 export const CoffeeList = () => {
   return (
     <CoffeeListExternalContainer>
       <CoffeeListInternalContainer>
-        {CoffeeData.map((coffee) => {
-          return <Coffee key={coffee.id} />
-        })}
+        <CoffeeText>
+          <h1>Nossos cafés</h1>
+        </CoffeeText>
+
+        <AllCoffees>
+          {CoffeeData.map((coffee) => {
+            return (
+              <Coffee
+                key={coffee.id}
+                img={coffee.img}
+                name={coffee.name}
+                tag1={coffee.tag1}
+                tag2={coffee.tag2}
+                tag3={coffee.tag3}
+                tag4={coffee.tag4}
+                tag5={coffee.tag5}
+                text={coffee.text}
+                price={coffee.price}
+                quantity={coffee.quantity}
+              />
+            )
+          })}
+        </AllCoffees>
       </CoffeeListInternalContainer>
     </CoffeeListExternalContainer>
   )
