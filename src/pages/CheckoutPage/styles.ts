854c8245e514a8236863c513b553cbba8f8bc35d
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-07-12 11:53:52 -0300

    feature/update-styled-selectedCoffee

--- src/pages/CheckoutPage/styles.ts
+++ src/pages/CheckoutPage/styles.ts
@@ -1,6 +1,33 @@
 import styled from 'styled-components'
 
-export const CheckoutContainer = styled.div``
+export const CheckoutContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding: 0rem 10rem 0rem;
+
+  width: 100%;
+
+  @media screen and (max-width: 780px) {
+    padding: 0% 7.22% 0%;
+    flex-direction: column;
+
+    font-size: 0.7rem;
+  }
+
+  @media screen and (max-width: 460px) {
+    padding: 0% 5% 0%;
+    flex-direction: column;
+    font-size: 0.6rem;
+  }
+
+  @media screen and (max-width: 286px) {
+    padding: 0% 2% 0%;
+    flex-direction: column;
+    font-size: 0.5rem;
+  }
+`
 
 export const DeliveryFormContainer = styled.div``
 
