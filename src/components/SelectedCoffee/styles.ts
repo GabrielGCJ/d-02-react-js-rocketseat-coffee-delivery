244e71b149ae3672fadceddf02abf48bd438dab9
gabrielj <gabriel.ggcj@gmail.com>
2023-07-12 00:32:59 -0300

    feature/add-selected-coffee

---
+++ src/components/SelectedCoffee/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const SelectedCoffeeContainer = styled.div``
+
+export const ImgContainer = styled.div``
+
+export const TitleAndButtonsContainer = styled.div``
+
+export const TitleContainer = styled.div``
+
+export const QuantityContainer = styled.div``
+
+export const RemoveButtonContainer = styled.div``
