189f99e574870a5bfe5e6db0775d7432db831f65
gabrielj <gabriel.ggcj@gmail.com>
2023-07-18 14:45:41 -0300

    feature/add feature page

--- src/pages/SuccessPage/SucessPage.tsx
+++ src/pages/SuccessPage/SucessPage.tsx
@@ -1,3 +1,67 @@
+import { useContext } from 'react'
+import location from '../../assents/IconLocationSucessPage.svg'
+import timer from '../../assents/IconTimerSucessPage.svg'
+import cash from '../../assents/IconCashSucessPage.svg'
+
+import {
+  DeliveryInformations,
+  FormOfPaymentInformationContainer,
+  InformationsAndImg,
+  LocationInformationContainer,
+  SucessPageContainer,
+  TextContainer,
+  TextInformation,
+  TimerInformationContainer,
+} from './styles'
+import ilustration from '../../assents/Illustration.svg'
+import { CoffeeContext } from '../../contexts/CoffeeContext'
+
 export const SuccessPage = () => {
-  return <h1>Success</h1>
+  const { addressUser } = useContext(CoffeeContext)
+  return (
+    <SucessPageContainer>
+      <TextContainer>
+        <h1>Uhu! Pedido Confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </TextContainer>
+      <InformationsAndImg>
+        <DeliveryInformations>
+          <LocationInformationContainer>
+            <img src={location} alt="" />
+            <TextInformation>
+              <p>
+                Entrega em{' '}
+                <strong>
+                  {addressUser.road}, {addressUser.numberHouse}{' '}
+                </strong>
+              </p>
+              <p>
+                {addressUser.district} - {addressUser.city}, {addressUser.uf}
+              </p>
+            </TextInformation>
+          </LocationInformationContainer>
+          <TimerInformationContainer>
+            <img src={timer} alt="" />
+            <TextInformation>
+              <p>Previsão de entrega</p>
+              <p>
+                <strong>20 min - 30 min</strong>
+              </p>
+            </TextInformation>
+          </TimerInformationContainer>
+          <FormOfPaymentInformationContainer>
+            <img src={cash} alt="" />
+            <TextInformation>
+              <p>Pagamento na entrega</p>
+              <p>
+                <strong>Cartão de Crédito</strong>
+              </p>
+            </TextInformation>
+          </FormOfPaymentInformationContainer>
+        </DeliveryInformations>
+
+        <img src={ilustration} alt="" />
+      </InformationsAndImg>
+    </SucessPageContainer>
+  )
 }
